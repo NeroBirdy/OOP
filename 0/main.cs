553759cc4089230174250393feaf8561886c0736
NeroBirdy <nirvanav.39@gmail.com>
2023-09-26 19:32:10 +0500

    .

---
+++ 0/main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace script
+{
+    class script
+    {
+
+        static void Main() 
+        {
+            Console.WriteLine("1");
+        }
+    }
+
+}
